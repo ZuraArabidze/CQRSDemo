cc2a8601a52ef171e125e2fab77f274b1e317033
ZuraArabidze <z.arabidzee@gmail.com>
2025-12-09 02:05:51 +0400

    setup postgres context and docker environment

---
+++ CQRSDemo/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using CQRSDemo.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CQRSDemo.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+    public DbSet<Product> Products => Set<Product>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Name).IsRequired().HasMaxLength(200);
+            entity.Property(e => e.Price).IsRequired().HasPrecision(18,2);
+        });
+    }
+}
