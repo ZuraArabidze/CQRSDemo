f28e514d54d5fe32c9f320069fca163f41804304
ZuraArabidze <z.arabidzee@gmail.com>
2025-12-10 00:46:56 +0400

    Project structure and packages

---
+++ CQRSDemo/Program.cs
@@ -0,0 +1,8 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+app.Run();
