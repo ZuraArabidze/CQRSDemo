3d559a3e95c25ed4a23c47f9f1c09b44e246718f
ZuraArabidze <z.arabidzee@gmail.com>
2025-12-10 02:02:52 +0400

    implement products controller with cqrs integration

---
+++ CQRSDemo/Controllers/ProductsController.cs
@@ -0,0 +1,77 @@
+﻿using CQRSDemo.Features.Products.Commands.CreateProduct;
+using CQRSDemo.Features.Products.Commands.DeleteProduct;
+using CQRSDemo.Features.Products.Commands.UpdateProduct;
+using CQRSDemo.Features.Products.Queries.GelAllProducts;
+using CQRSDemo.Features.Products.Queries.GetProductById;
+using CQRSDemo.Models.DTOs;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CQRSDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProductsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct([FromBody] CreateProductRequest request)
+        {
+            var command = new CreateProductCommand
+            {
+                Name = request.Name,
+                Price = request.Price,
+                Stock = request.Stock
+            };
+
+            var id = await _mediator.Send(command);
+            return Ok(id);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProduct(int id, [FromBody] UpdateProductRequest request)
+        {
+            var command = new UpdateProductCommand
+            {
+                Id = id,
+                Name = request.Name,
+                Price = request.Price,
+                Stock = request.Stock
+            };
+
+            var result = await _mediator.Send(command);
+            return result ? NoContent() : NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var command = new DeleteProductCommand { Id = id };
+            var result = await _mediator.Send(command);
+            return result ? NoContent() : NotFound();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductById(int id)
+        {
+            var query = new GetProductByIdQuery { Id = id };
+            var product = await _mediator.Send(query);
+            return product != null ? Ok(product) : NotFound();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            var query = new GetAllProductsQuery();
+            var products = await _mediator.Send(query);
+            return Ok(products);
+        }
+    }
+}
